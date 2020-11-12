df629c1df637dee67605be1c2298b89f72e9957c
Kumar Nikhil <knikhil352000@gmail.com>
2020-11-12 22:45:28 +0530

    Added data to the firestore using form

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const cafeList = document.querySelector('#cafe-list');
-
+const form = document.querySelector("#add-cafe-form");
 // create element and render cafe
 
 function renderCafe(doc){
     let li = document.createElement('li');
     let name = document.createElement('span');
-    let city = document.createElement('span')
+    let city = document.createElement('span');
+    let cross = document.createElement('div');
+    cross.textContent = 'x';
     li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
     city.textContent = doc.data().city;
     li.appendChild(name);
     li.appendChild(city);
+    li.appendChild(cross);
     cafeList.appendChild(li);
+
+    //
 }
 
 db.collection('cafes').get().then((snapshot) => {
@@ -19,4 +24,14 @@ db.collection('cafes').get().then((snapshot) => {
         console.log(doc.data());
         renderCafe(doc);
     });
+});
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    });
+    form.name.value = '';
+    form.city.value = '';
 });
